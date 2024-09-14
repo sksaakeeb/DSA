afb494cb8b90bea0117e5a94373400fcc1ffae98
Sk Sakib <109467015+sksaakeeb@users.noreply.github.com>
2024-09-14 23:53:16 +0530

    BST Operations

---
+++ BinarySearchTree/BST.java
@@ -0,0 +1,51 @@
+package BinarySearchTree;
+
+public class BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        }
+        else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // in-order traversal
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
+    }
+
+    public static void main(String [] args) {
+        int[] values = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+
+        for (int i=0; i<values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inOrder(root);
+    }
+}
