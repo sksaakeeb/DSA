6e114162dec2e6a67cdf1a62b47b86a7e7a03be2
Sk Sakib <109467015+sksaakeeb@users.noreply.github.com>
2024-09-16 21:52:01 +0530

    BST Operations

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -11,6 +11,7 @@ public class BST {
         }
     }
 
+    // insert node on a BST or build BST
     public static Node insert(Node root, int val) {
         if (root == null) {
             root = new Node(val);
@@ -38,6 +39,60 @@ public class BST {
         inOrder(root.right);
     }
 
+    // search in a BST - O(H)
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key); // left subtree
+        }
+        else if (root.data == key) {
+            return true;
+        }
+        else {
+            return search(root.right, key); // right subtree
+        }
+    }
+
+    // delete a node
+    public static Node delete(Node root, int key) {
+        if (root.data > key) {
+            root.left = delete(root.left, key);
+        } else if (root.data < key) {
+            root.right = delete(root.right, key);
+        }
+        else {
+            // case-1
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case-2
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case-3
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    // finding in-order successor
+    public static Node inorderSuccessor(Node root) {
+        while (root.left != null) { // left most node of right subtree
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String [] args) {
         int[] values = {5, 1, 3, 4, 2, 7};
         Node root = null;
@@ -46,6 +101,14 @@ public class BST {
             root = insert(root, values[i]);
         }
 
+        inOrder(root);
+        if (search(root, 3)) {
+            System.out.println("Node found");
+        } else {
+            System.out.println("Node not found");
+        }
+
+        delete(root, 5);
         inOrder(root);
     }
 }
